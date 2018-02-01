d28b6e5cee3bc3a6bfb56d58046b353a1e546f31
Krzesimir Siatka <k.siatka@gmail.com>
2018-02-01 20:00:00 +0000

    Added logging request details to mongo.

--- api-app/routes.js
+++ api-app/routes.js
@@ -1,22 +1,45 @@
 var faker = require("faker");
+var db = require("./db");
+
+var log = function(req) {
+    db.get().collection("logs").insert({
+        hostname: req.hostname,
+        method: req.method,
+        url: req.originalUrl,
+        ip: req.ip,
+        date: Date.now()
+    }, function(err, result) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(result);
+        }
+    });
+};
 
 var appRouter = function (app) {
 
   app.get("/", function (req, res) {
+    log(req);
     res.status(200).send({ message: 'Welcome to our restful API' });
   });
 
   app.get("/user", function (req, res) {
+    log(req);
+      
     var data = ({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       username: faker.internet.userName(),
       email: faker.internet.email()
     });
+    
     res.status(200).send(data);
   });
 
  app.get("/users/:num", function (req, res) {
+   log(req);
+     
    var users = [];
    var num = req.params.num;
 
@@ -33,7 +56,7 @@ var appRouter = function (app) {
      res.status(200).send(users);
     
    } else {
-     res.status(400).send({ message: 'invalid number supplied' });
+     res.status(400).send({ message: "Invalid number supplied." });
    }
 
  });
